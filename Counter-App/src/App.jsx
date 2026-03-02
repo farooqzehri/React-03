2a14a06900bd7d32b372b16c830cfd26863840df
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-03-02 20:59:45 +0500

    usestate

---
+++ Counter-App/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [counter , setCounter] = useState(0)
+  return (
+    <>
+    <h1>Counter App</h1>
+    <div>
+    <button onClick={()=>setCounter(counter + 1)}>+</button>
+      <h1>{counter}</h1>
+    <button>-</button>
+    </div>
+    </>
+  )
+}
+
+export default App
