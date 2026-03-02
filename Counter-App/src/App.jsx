9ce3afdca48fc07f1915a6623a9a3cff330198d0
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-03-03 01:45:08 +0500

    style

--- Counter-App/src/App.jsx
+++ Counter-App/src/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [counter , setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
   return (
     <>
-    <h1>Counter App</h1>
-    <div>
-    <button  onClick={()=>setCounter(counter + 1)}>+</button>
-      <h1>{counter}</h1>
-    <button   onClick={()=>setCounter(counter - 1)}>-</button>
-    <button onClick={()=>setCounter(0)}>reset</button>
-    </div>
+      <h1>Counter App</h1>
+      <div>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(counter - 1)}>-</button>
+        <button onClick={() => setCounter(0)}>reset</button>
+      </div>
     </>
   )
 }
