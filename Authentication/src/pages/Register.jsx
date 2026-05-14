a90e7da7996097eee025d098add8e815f0a2682d
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-05-15 01:46:24 +0500

    feat: add confirm password field to registration form

--- Authentication/src/pages/Register.jsx
+++ Authentication/src/pages/Register.jsx
@@ -7,6 +7,8 @@ function Register() {
         <input type="text"  placeholder='Username'/>
         <input type="email" placeholder='Email' />
         <input type="password" placeholder='Password' />
+        <input type="password" placeholder='Confirm Password' />
+        <button type="submit">Register</button>
     </form>
     </>
   )
