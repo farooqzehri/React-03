015fb43688934c23d92e060b970b110a9efdc896
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-05-19 17:42:00 +0500

    users added custom

---
+++ 10-Practice/src/pages/Users.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Users() {
+    const customers = [
+        { 
+            id: 1, name: 'John Doe' ,
+           email: 'john.doe@example.com',
+           phone: '123-456-7890', address: '123 Main St, Anytown, USA',
+           image: 'https://randomuser.me/api/portraits/men/1.jpg' 
+
+        },
+        {
+            id: 2, name: 'Jane Smith' ,
+            email: 'janesmith@example.com',
+            phone: '987-654-3210', address: '456 Elm St, Othertown, USA',
+            image: 'https://randomuser.me/api/portraits/women/2.jpg'
+        },
+        {
+            id: 3, name: 'Bob Johnson' ,
+            email: 'bobjon@example.com',
+            phone: '555-555-5555', address: '789 Oak St, Another town, USA',
+            image: 'https://randomuser.me/api/portraits/men/3.jpg'
+        },
+        {
+            id: 4 , name: 'Alice Williams' ,
+            email: 'alice@example.com',         
+            phone: '555-123-4567', address: '101 Pine St, Yet Another Town, USA',
+            image: 'https://randomuser.me/api/portraits/women/4.jpg'
+        }
+
+    ]
+  return (
+    <div>Users
+        <ul>
+            {customers && customers.map(item => {
+                return <div key={item.id}>
+                    <img src={item.image} alt={item.name} width="100" height="100"/>
+                    <h3>{item.name}</h3>
+                    <p>Email: {item.email}</p>
+                    <p>Phone: {item.phone}</p>
+                    <p>Address: {item.address}</p>
+                </div>
+            }) }
+        </ul>
+    </div>
+  )
+}
+
+export default Users
