2c7b4ad8ba908f95e71932637a5c30e7a1f20377
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-05-20 23:31:59 +0500

    details added

--- 10-Practice/src/pages/SingleUser.jsx
+++ 10-Practice/src/pages/SingleUser.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function SingleUser() {
+     const customers = [
+        { 
+            id: 1, name: 'John Doe' ,
+           email: 'john.doe@example.com',
+           phone: '123-456-7890', address: '123 Main St, Anytown, USA',
+           image: 'https://randomuser.me/api/portraits/men/1.jpg' 
+
+        },
+        {
+            id: 2, name: 'Jane Smith' ,
+            email: 'janesmith@example.com',
+            phone: '987-654-3210', address: '456 Elm St, Othertown, USA',
+            image: 'https://randomuser.me/api/portraits/women/2.jpg'
+        },
+        {
+            id: 3, name: 'Bob Johnson' ,
+            email: 'bobjon@example.com',
+            phone: '555-555-5555', address: '789 Oak St, Another town, USA',
+            image: 'https://randomuser.me/api/portraits/men/3.jpg'
+        },
+        {
+            id: 4 , name: 'Alice Williams' ,
+            email: 'alice@example.com',         
+            phone: '555-123-4567', address: '101 Pine St, Yet Another Town, USA',
+            image: 'https://randomuser.me/api/portraits/women/4.jpg'
+        }
+
+    ]
+    const {id} = useParams()
+    // const customer = customers.find(item => item.id === parseInt(id))
+
   return (
-    <div>SingleUser</div>
+    <div>SingleUser
+
+        {customers && <div key={customers[id-1].id}>
+                    <img src={customers[id-1].image} alt={customers[id-1].name} width="100" height="100"/>
+                    <h3>{customers[id-1].name}</h3> </div>}
+    </div>
+    
   )
 }
 
