24d6321e4cad8c47756c4701c25659108bf02302
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-03 00:34:21 +0500

    Form

--- 10-Practice/src/pages/About.jsx
+++ 10-Practice/src/pages/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>About
+
+      <form>
+        <input type="" />
+        
+      </form>
+    </div>
   )
 }
 
