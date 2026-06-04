d538675dbcdfeb143cf3cc7e61d949b0a5fb3a92
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-05 00:49:02 +0500

    variable added

--- 10-Practice/src/pages/About.jsx
+++ 10-Practice/src/pages/About.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function About() {
   const [input , setInput] = useState('')
   const [ todos , setTodos] = useState([])
+  const []
 
   const addTodo = (e) => {
     e.preventDefault()
@@ -14,7 +15,7 @@ function About() {
 
       <form onSubmit={addTodo}>
         <input type="text" placeholder='enter Your Todo'  value={input} onChange={e => setInput(e.target.value)} />
-        <input type="text" placeholder='Enter Your descriptions' value={input2} onChange={} />
+        <input type="text" placeholder='Enter Your descriptions' value={input2} onChange={e => setInput2(e.target.value)} />
 
         <button>add Todo</button>
         </form>
