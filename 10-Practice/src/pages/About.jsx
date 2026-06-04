b1af785fe8572de75fce27af7f83deaa42b90eed
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-05 00:47:26 +0500

    value added

--- 10-Practice/src/pages/About.jsx
+++ 10-Practice/src/pages/About.jsx
@@ -14,7 +14,8 @@ function About() {
 
       <form onSubmit={addTodo}>
         <input type="text" placeholder='enter Your Todo'  value={input} onChange={e => setInput(e.target.value)} />
-        <input type="text" placeholder='Enter Your descriptions' />
+        <input type="text" placeholder='Enter Your descriptions' value={} />
+
         <button>add Todo</button>
         </form>
         <div>
