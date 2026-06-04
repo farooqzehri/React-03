5206ecaa6a2069779c3acddb5d648a53ef03461c
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-04 23:32:12 +0500

    making a new Todo

--- 10-Practice/src/pages/About.jsx
+++ 10-Practice/src/pages/About.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function About() {
+  const [input , setInput] = useState('')
+  const [ todos , setTodos] = useState([])
   return (
     <div>About
 
       <form>
-        <input type="email" placeholder='enter Your Email'  />
+        <input type="text" placeholder='enter Your Email'  />
+        <button>add Todo</button>
         
       </form>
     </div>
