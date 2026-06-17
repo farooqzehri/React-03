9b873f1d73accb4205b91d2c5a6eed82717678ca
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-17 18:33:18 +0500

    usere name added

--- 10-Practice/src/pages/Users.jsx
+++ 10-Practice/src/pages/Users.jsx
@@ -29,7 +29,7 @@ function Users() {
             image: 'https://randomuser.me/api/portraits/women/4.jpg'
         },
         {
-            id:5, 
+            id:5,  name: 
         }
         
 
