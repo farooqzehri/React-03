659e7feb69cfc67a69e2f5dd08970c18c5122e14
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-20 00:16:38 +0500

    main jsx

--- 10-Practice/src/main.jsx
+++ 10-Practice/src/main.jsx
@@ -6,6 +6,9 @@ import About from './pages/About.jsx'
 import Users from './pages/Users.jsx'
 import Navbar from './components/Navbar.jsx'
 import SingleUser from './pages/SingleUser.jsx'
+import Todos from './pages/Todos.jsx'
+import ResourceForm from './pages/ResourceForm.jsx'
+import ResourceCard from './pages/ResourceCard.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
@@ -15,6 +18,9 @@ createRoot(document.getElementById('root')).render(
   <Route path="/about" element={<About/>}/>
   <Route path="/users" element={<Users/>}/>
   <Route path='/singleuser/:id' element={<SingleUser/>}/>
+  <Route path='/todos' element={<Todos/>}/>
+  <Route path='/resourceform' element={<ResourceForm/>}/>
+  <Route path='/resourcecard' element={<ResourceCard/>}/>
 </Routes>
 </BrowserRouter>
 )
