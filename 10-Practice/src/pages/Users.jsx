1837c38d635581b6d317cb4019a830d987495a6c
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-07-20 10:57:06 +0500

    new user id

--- 10-Practice/src/pages/Users.jsx
+++ 10-Practice/src/pages/Users.jsx
@@ -86,7 +86,10 @@ function Users() {
                 email: 'lisa.simpson@example.com',
                 phone: '555-555-2342', address: '1010 Magnolia St, Last Town, USA',
                 image: 'https://randomuser.me/api/portriats/women/13.jpg'
-                
+
+            },
+            {
+                id: 14,
             }
         
 
